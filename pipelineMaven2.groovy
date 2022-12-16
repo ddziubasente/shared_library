9bc70fdbf67c3d21a869b562df006c0a69d0c013
ddziubasente <120571868+ddziubasente@users.noreply.github.com>
2022-12-16 12:09:42 +0100

    Create pipelineMaven2.groovy

---
+++ pipelineMaven2.groovy
@@ -0,0 +1,8 @@
+@Library('my-shared-library@master') _
+
+buildMaven(
+    node: "slave01-ubuntu",
+    gitUrl: "https://github.com/jenkins-docs/simple-java-maven-app.git",
+    branch: "master",
+    deliverScriptPath: "./jenkins/scripts/deliver.sh"
+)
